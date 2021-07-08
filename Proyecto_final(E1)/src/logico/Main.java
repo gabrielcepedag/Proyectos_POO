17292f31618bfa5d8c2c3d6da9b85c0daaef84d6
darvy <darvy@Darvy-Betances>
2021-07-08 00:11:59 -0400

    Probando

--- Proyecto_final(E1)/src/logico/Main.java
+++ Proyecto_final(E1)/src/logico/Main.java
@@ -1,9 +1,21 @@
 package logico;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public Main() {
-		// Y ahora?
+		int num1 = 0, num2 = 0, resultado = 0;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Ingrese el primer valor: ");
+		num1 = entrada.nextInt();
+		
+		System.out.println("Ingrese el segundo valor: ");
+		num2 = entrada.nextInt();
+		
+		resultado = num1 + num2;
+		System.out.println("Su resultado es " + resultado + " Coño");
+	
 	}
-
 }
