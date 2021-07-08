b29ba6657ae4b6c83bd73fb0238662de1fd60795
darvy <darvy@Darvy-Betances>
2021-07-07 23:30:51 -0400

    ClaseB Prueba (Darvy)

---
+++ Proyecto_final(E1)/src/logico/ClaseB.java
@@ -0,0 +1,14 @@
+package logico;
+
+public class ClaseB {
+	
+	public ClaseB() {
+		boolean aburrio = false;
+		boolean comida = false;
+			
+			if (comida == false) {
+				aburrio = true;
+			}
+	}
+
+}
